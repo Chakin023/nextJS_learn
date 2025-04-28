171db7dd2af0b9aafa8c743258ea207e0402d7dc
Chakins023 <chakin.pattanasombatsakul@gmail.com>
2025-04-28 14:24:42 +0700

    import/add font and add sample page

---
+++ src/app/contact/page.tsx
@@ -0,0 +1,10 @@
+ export default function ContractPage() {
+   return (
+     <main>
+       <h1 className="bg-amber-50">Hello contract page</h1>
+       <p className="font-k2d">สวัสดีครับ K2D</p>
+       <p className="font-kanit">สวัสดีครับ Kanit</p>
+       <p className="font-sarabun">สวัสดีครับ Sarabun</p>
+     </main>
+   );
+ }
